84dfc7729f1b67290d39ac44cd3bdc6b88ce4756
Hou Rui <houruinus@gmail.com>
2024-08-24 22:28:37 +0800

    Refactor project structure

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ PREFIX="${1:-/usr}"
 PROJECT='kadwsettings'
 TMPRUN='run.sh'
 
-SHARE_DIR="$PREFIX/share/$PROJECT"
+SRC_DIR="$PREFIX/share/$PROJECT/src"
 ICON_DIR="$PREFIX/share/icons/hicolor"
 APPL_DIR="$PREFIX/share/applications"
 
-install -Dm 644 ./*.py -t "$SHARE_DIR"
-install -Dm 644 ./*.qml -t "$SHARE_DIR"
-install -Dm 644 ./*.js -t "$SHARE_DIR"
+install -Dm 644 src/*.py -t "$SRC_DIR"
+install -Dm 644 src/*.qml -t "$SRC_DIR"
+install -Dm 644 src/*.js -t "$SRC_DIR"
 install -Dm 644 ./*.desktop -t "$APPL_DIR"
 
 for size in icons/*; do
@@ -18,7 +18,6 @@ for size in icons/*; do
   install -Dm 644 "icons/$sub_dir/"* -t "$ICON_DIR/$sub_dir"
 done
 
-echo '#!/bin/sh' >"$TMPRUN"
-echo "python \"$SHARE_DIR/main.py\"" >> "$TMPRUN"
+printf "#!/bin/sh\npython \"%s\"" "$SRC_DIR/main.py" >"$TMPRUN"
 install -Dm 755 "$TMPRUN" -T "$PREFIX/bin/$PROJECT"
 rm "$TMPRUN"
